f3a7a79b976668a1f8eb1c9d9f08c47f7ecfb8e1
0xKubitus <n.cubilier@gmail.com>
2022-04-28 14:37:09 +0200

    folder created

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'bundler'
+Bundler.require
+
+require 'lib/player' 
+require 'lib/board' 
+require 'lib/boardcase'
+require 'lib/game'
+require 'views/show'
+#etc # <= remplacer par nom de classe
+
+
+#Le code de l'appli commence ici :
+MyClass.new.perform # <= à modifier aussi, évidemment.
---
+++ spec/app_spec.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/app'
+
+#template => https://www.thehackingproject.org/fr/dashboard/courses/4/lessons/13
